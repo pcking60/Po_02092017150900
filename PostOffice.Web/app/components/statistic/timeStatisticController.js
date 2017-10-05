b35cc68074ef2e3a11d3c86bb924cfbf12336e86
pcking60 <thientt.bdst@gmail.com>
2017-10-05 20:42:53 +0700

    performance-ng-repeat

--- PostOffice.Web/app/components/statistic/timeStatisticController.js
+++ PostOffice.Web/app/components/statistic/timeStatisticController.js
@@ -4,6 +4,7 @@
     timeStatisticController.$inject = ['$scope', 'apiService', 'notificationService', '$filter', 'authService', '$stateParams'];
 
     function timeStatisticController($scope, apiService, notificationService, $filter, authService, $stateParams) {
+        $scope.loading = true;
         $scope.statisticResult = [];
         $scope.report = {
             date: { startDate: moment(), endDate: moment() },
@@ -30,12 +31,12 @@
                     $scope.report.districts = response.data;
                 },
                 function (error) {
-                    notificationService.displayError('Không lấy được danh sách đơn vị / huyện')
+                    notificationService.displayError('Không lấy được danh sách đơn vị / huyện');
                 });
         }
         // lấy danh sách bưu cục của đơn vị được chọn
         $scope.updatePos = function (item) {
-            if(item!=0 && item!=null){
+            if(item!==0 && item!==null){
                 $stateParams.id = item;                
                 getPos();
             }
@@ -46,7 +47,7 @@
         };
         // lấy danh sách users của bưu cục được chọn
         $scope.updateUser = function (item) {
-            if (item != 0 && item != null) {
+            if (item !== 0 && item !== null) {
                 $stateParams.id = item;
                 getListUser();
             }
@@ -57,7 +58,7 @@
         };
         // lấy danh sách dịch vụ user đã thực hiện
         $scope.updateService = function (item) {
-            if (item != 0 && item != null) {
+            if (item !== 0 && item !== null) {
                 $stateParams.id = item;
                 getService();
             }
@@ -113,6 +114,14 @@
 
         $scope.TimeStatistic = TimeStatistic;
         function TimeStatistic() {
+            $scope.loading = true;
+            $scope.result = false;
+            $scope.statisticResult = null;
+            $scope.report.totalQuantity = 0;
+            $scope.report.totalCash = 0;
+            $scope.report.totalDebt = 0;
+            $scope.report.totalEarn = 0;
+            $scope.report.totalVat = 0;
             var fromDate = $scope.report.date.startDate.format('MM-DD-YYYY');
             var toDate = $scope.report.date.endDate.format('MM-DD-YYYY');
             var config = {
@@ -123,36 +132,42 @@
                     districtId: $scope.report.districtId || 0,
                     posId: $scope.report.posId || 0,
                     userId: $scope.report.userId || '',
-                    serviceId: $scope.report.serviceId || 0                    
+                    serviceId: $scope.report.serviceId || 0
                 }
-            }
+            };
             apiService.get('api/transactions/stattistic', config,
                 function (response) {
-                    $scope.statisticResult = response.data;
-                    $scope.report.totalQuantity = 0;
-                    $scope.report.totalCash = 0;
-                    $scope.report.totalDebt = 0;
-                    $scope.report.totalEarn = 0;
-                    $scope.report.totalVat = 0;
-                    angular.forEach($scope.statisticResult, function (item) {
-                        if (item.Status == true) {
-                            $scope.report.totalQuantity += item.Quantity;
-                            $scope.report.totalCash += item.TotalCash;
-                            $scope.report.totalDebt += item.TotalDebt;
-                            $scope.report.totalEarn += item.EarnMoney;
-                            $scope.report.totalVat += (item.TotalCash + item.TotalDebt) -((item.TotalCash + item.TotalDebt) / item.VAT);
-                        }                        
-                    })
-                    $scope.result = true;
-                    console.log(response.data);                    
+                    if (response.data.length === 0) {
+                        notificationService.displayError('Không có dữ liệu');                        
+                    }
+                    else {
+                        $scope.result = true;
+                        $scope.statisticResult = response.data;
+                        $scope.report.totalQuantity = 0;
+                        $scope.report.totalCash = 0;
+                        $scope.report.totalDebt = 0;
+                        $scope.report.totalEarn = 0;
+                        $scope.report.totalVat = 0;
+                        angular.forEach($scope.statisticResult, function (item) {
+                            if (item.Status === true) {
+                                $scope.report.totalQuantity += item.Quantity;
+                                $scope.report.totalCash += item.TotalCash;
+                                $scope.report.totalDebt += item.TotalDebt;
+                                $scope.report.totalEarn += item.EarnMoney;
+                                $scope.report.totalVat += item.TotalCash + item.TotalDebt - (item.TotalCash + item.TotalDebt) / item.VAT;
+                            }
+                        });
+                    }
+                    $scope.loading = false;
                 },
                 function (response) {
-                    if (response.status == 500) {
+                    if (response.data.length === 0) {
                         notificationService.displayError('Không có dữ liệu');
                     }
                     else {
                         notificationService.displayError('Không thể tải dữ liệu');
                     }
+                    $scope.loading = false;
                 });
         }
         //check role 
@@ -180,7 +195,7 @@
                         apiService.get('/api/po/getbyid/' + response.data.POID, null, function (result) {
                             $stateParams.id = result.data.DistrictID;
                             getPos();
-                        }, function (error) { })                       
+                        }, function (error) { });
                     },
                     function (response) {
                         notificationService.displayError('Không tải được danh sách dịch vụ.');
